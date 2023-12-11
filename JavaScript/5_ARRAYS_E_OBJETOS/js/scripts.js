730682ed0c3e17f21096543a2fa0f04d9a145ffd
OrlandoVieira <orlandovieiradev@gmail.com>
2023-12-11 11:03:35 -0300

    Feat - finalizando funç | inic Arrays e Objetos

---
+++ JavaScript/5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -0,0 +1,261 @@
+// // 1 - arrays
+// const lista = [1, 2, 3, 4, 5];
+
+// console.log(lista);
+
+// console.log(typeof lista);
+
+// const itens = ["Orlando", true, 2, 4.22, []];
+
+// console.log(itens);
+
+// // 2 - mais sobre arrays
+// const arr = ["a", "b", "c", "d", "e"];
+
+// console.log(arr[0]);
+
+// console.log(arr[2]);
+
+// console.log(arr[83]);
+
+// // 3 - propriedades
+// const numbers = [5, 3, 4];
+
+// console.log(numbers.length);
+
+// console.log(numbers["length"]);
+
+// const myName = "Orlando";
+
+// console.log(myName.length);
+
+// // 4 - métodos
+// const otherNumbers = [1, 2, 3];
+
+// const allNumbers = numbers.concat(otherNumbers);
+
+// console.log(allNumbers);
+
+// const text = "algum texto";
+
+// console.log(text.toUpperCase());
+
+// console.log(typeof text.toUpperCase);
+
+// console.log(text.indexOf("g"));
+
+// // 5 - objetos
+// const person = {
+//   name: "Orlando",
+//   Age: 34,
+//   job: "Programador",
+// };
+
+// console.log(person);
+
+// console.log(person.name);
+
+// console.log(person.name.length);
+
+// console.log(typeof person);
+
+// // 6 - criando e deletando propriedades
+// const car = {
+//   engine: 1.6,
+//   brand: "VW",
+//   model: "Polo",
+//   km: 20000,
+// };
+
+// console.log(car);
+
+// car.doors = 4;
+
+// console.log(car);
+
+// delete car.km;
+
+// console.log(car);
+
+// // 7 - mais sobre objetos
+// const obj = {
+//   a: "teste",
+//   b: true,
+// };
+
+// console.log(obj instanceof Object);
+
+// const obj2 = {
+//   c: [],
+// };
+
+// Object.assign(obj2, obj);
+
+// console.log(obj2);
+
+// console.log(obj);
+
+// //  8 - conhecendo melhor os objetos
+// console.log(Object.keys(obj));
+// console.log(Object.keys(obj2));
+// console.log(Object.keys(car));
+
+// console.log(Object.entries(car));
+
+// // 9 - Mutação
+// const a = {
+//   name: "Orlando",
+// };
+
+// const b = a;
+// console.log(a);
+// console.log(b);
+
+// console.log(a === b);
+
+// a.age = 34;
+
+// console.log(a);
+// console.log(b);
+
+// delete b.age;
+
+// console.log(a);
+// console.log(b);
+
+// // 10 - loop em array
+// const users = ["Orlando", "João", "Pedro", "Miguel"];
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(`Listando o usuário: ${users[i]}`);
+// }
+
+// // 11 - push e pop
+// const array = ["a", "b", "c"];
+
+// array.push("d");
+
+// console.log(array);
+
+// console.log(array.length);
+
+// array.pop();
+
+// console.log(array);
+
+// const itemRemovido = array.pop();
+
+// console.log(itemRemovido);
+
+// console.log(array);
+
+// array.push("z", "x", "y");
+
+// console.log(array);
+
+// // 12 - shift e unshift
+// const letters = ["a", "b", "c"];
+
+// const letter = letters.shift();
+
+// console.log(letter);
+
+// console.log(letters);
+
+// letters.unshift("p", "q", "r");
+
+// letters.unshift("z");
+
+// console.log(letters);
+
+// // 13 - indexOf e lastIndexOf
+// const myElements = ["Morango", "Maçã", "Abacate", "Pêra", "Abacate"];
+
+// console.log(myElements[2]);
+// console.log(myElements[myElements.indexOf("Abacate")]);
+
+// console.log(myElements.lastIndexOf("Abacate"));
+
+// console.log(myElements.indexOf("Mamão"));
+
+// console.log(myElements.lastIndexOf("Mamão"));
+
+// // 14 - slice
+// const testeSlice = ["a", "b", "c", "d", "e", "f"];
+
+// const subArray = testeSlice.slice(2, 4);
+
+// console.log(subArray);
+
+// console.log(testeSlice);
+
+// const subArray2 = testeSlice.slice(2, 4 + 1);
+
+// console.log(subArray2);
+
+// const subArray3 = testeSlice.slice(10, 20);
+
+// console.log(subArray3);
+
+// const subArray4 = testeSlice.slice(2);
+
+// console.log(subArray4);
+
+// // 15 - foreach
+// const nums = [1, 2, 3, 4, 5];
+
+// nums.forEach((numero) => {
+//   console.log(`O número é ${numero}`);
+// });
+
+// const posts = [
+//   { title: "Primeiro post", category: "PHP" },
+//   { title: "Segundo post", category: "JavaScript" },
+//   { title: "Terceiro post", category: "Python" },
+// ];
+
+// posts.forEach((posts) => {
+//   console.log(`Exibindo: ${posts.title}, da categoria: ${posts.category}`);
+// });
+
+// // 16 - includes
+// const brands = ["BMW", "VW", "FIAT"];
+
+// console.log(brands.includes("FIAT"));
+
+// console.log(brands.includes("KIA"));
+
+// if (brands.includes("BMW")) {
+//   console.log("Há carros da marca BMW!");
+// }
+
+// // 17 - reverse
+// const reverseTest = [1, 2, 3, 4, 5];
+
+// reverseTest.reverse();
+
+// console.log(reverseTest);
+
+// 18 - Trim
+const trimTest = "   testando \n   ";
+
+console.log(trimTest);
+
+console.log(trimTest.trim());
+
+console.log(trimTest.length);
+
+console.log(trimTest.trim().length);
+
+// 19 - padstart
+const testePadStart = "1";
+
+const newNumber = testePadStart.padStart(4, "0");
+
+console.log(testePadStart);
+
+console.log(newNumber);
+
+const testPadEnd = newNumber.padEnd(10, "0");
+
+console.log(testPadEnd);
